edf2c3fca5ba2a1263d04e0cd4a9fd2fccfbc102
Erick <erickalexandrex@gmail.com>
2021-11-18 23:06:17 -0300

    first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,22 +1,5 @@
-import * as S from './styles';
-
 const Main: React.FC = () => {
-  return (
-    <S.Wrapper>
-      <S.Logo
-        src="/img/logo.svg"
-        alt="React logo with React Avançado written on it"
-      />
-      <S.Title>React Avançado</S.Title>
-      <S.Description>
-        Typescript, React, NextJS and Styled Components
-      </S.Description>
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="A developer guy in front of a code."
-      />
-    </S.Wrapper>
-  );
+  return <h1>NextJS na prática</h1>;
 };
 
 export default Main;
